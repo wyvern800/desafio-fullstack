1d519555f2826f9e56b9bf84765cca235f321a8f
wyvern800 <matheusguiferreira@hotmail.com>
2019-09-14 03:21:00 -0300

    Added 1 more entity to the CRUD

--- app/Http/Controllers/PessoaController.php
+++ app/Http/Controllers/PessoaController.php
@@ -19,7 +19,7 @@ class PessoaController extends Controller
 	public function novo(Request $request){
 		$data = sizeof($_POST) > 0 ? $_POST : json_decode($request->getContent(), true); // Pega o post ou o raw
 
-		$res = DB::insert('insert into tb_pessoa (nome, email, data_nasc, telefone, sexo, cep, nome_pai, nome_mae) values (?, ?, ?, ?, ?, ?, ?, ?)', [$data['nome'], $data['email'], $data['data_nasc'], $data['telefone'], $data['sexo'], $data['cep'], $data['nome_pai'], $data['nome_mae']]); // Insert
+		$res = DB::insert('insert into tb_pessoa (nome, email, data_nasc, telefone, sexo, cep, nome_pai, nome_mae, data_modificacao) values (?, ?, ?, ?, ?, ?, ?, ?, ?)', [$data['nome'], $data['email'], $data['data_nasc'], $data['telefone'], $data['sexo'], $data['cep'], $data['nome_pai'], $data['nome_mae'], $data['data_modificacao']]); // Insert
 
 		return ["status" => ($res)?'ok':'erro'];
 	}
@@ -32,7 +32,7 @@ class PessoaController extends Controller
         return ['id invalido'];
       }
   		$res = DB::update("update tb_pessoa set nome = ?,email = ?, data_nasc = ?, telefone = ?,
-      sexo = ?, cep = ?, nome_pai = ?, nome_mae = ? WHERE id = ?", [$data['nome'], $data['email'], $data['data_nasc'], $data['telefone'], $data['sexo'], $data['cep'], $data['nome_pai'], $data['nome_mae'], $data['id']]); // Update
+      sexo = ?, cep = ?, nome_pai = ?, nome_mae = ?, data_modificacao = ? WHERE id = ?", [$data['nome'], $data['email'], $data['data_nasc'], $data['telefone'], $data['sexo'], $data['cep'], $data['nome_pai'], $data['nome_mae'], $data['data_modificacao'], $data['id']]); // Update
 
   		return ["status" => ($res)?'ok':'erro'];
   	}
